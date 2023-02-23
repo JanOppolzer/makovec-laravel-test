274e488f87f3c907a06b1b319f3746c016f91ed2
Jan Oppolzer <jan@oppolzer.cz>
2023-02-23 16:41:03 +0100

    WIP

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\FakeController;
+use App\Http\Controllers\ShibbolethController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,14 +18,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return auth()->user() ? view('dashboard') : view('welcome');
-})->name('home');
-
 if (app()->environment(['local', 'testing'])) {
     Route::match(['get', 'post'], '/fakelogin/{id?}', [FakeController::class, 'login']);
     Route::get('fakelogout', [FakeController::class, 'logout']);
 }
+
+Route::get('/', function () {
+    return auth()->user() ? view('dashboard') : view('welcome');
+})->name('home');
+
+Route::get('blocked', function () {
+    return auth()->user() ? redirect('/') : view('blocked');
+});
 
 Route::resource('devices', DeviceController::class);
 Route::resource('categories', CategoryController::class);
@@ -34,6 +39,6 @@ Route::patch('users/{user}/role', [UserController::class, 'role'])->name('users.
 Route::patch('users/{user}/categories', [UserController::class, 'categories'])->name('users.categories');
 Route::resource('users', UserController::class)->except('edit', 'destroy');
 
-Route::get('login', function () {
-    return 'login';
-})->name('login')->middleware('guest');
+Route::get('login', [ShibbolethController::class, 'create'])->name('login')->middleware('guest');
+Route::get('auth', [ShibbolethController::class, 'store'])->middleware('guest');
+Route::get('logout', [ShibbolethController::class, 'destroy'])->name('logout')->middleware('auth');
