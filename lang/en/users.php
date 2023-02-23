2a6fd9dbaa7987a6647227b36652798f28fffdd1
Jan Oppolzer <jan@oppolzer.cz>
2023-02-23 18:01:13 +0100

    WIP

--- lang/en/users.php
+++ lang/en/users.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
+    'active' => ':name has been successfully activated.',
     'add' => 'Add user',
     'admined' => ':name has been successfully granted administrator rights.',
     'deadmined' => ':name has been successfully removed administrator rights.',
+    'inactive' => ':name has been successfully deactivated.',
     'none_found' => 'No user found.',
     'profile' => "Users's profile",
     'select_different_email' => 'You can choose a different email address from the following list:',
